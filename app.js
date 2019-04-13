3d2d567a0a0ccff2a23bde1160046b6f4bc4ed62
Samantha Ambroise <ilkigin@gmail.com>
2019-04-13 13:21:20 -0400

    Added validation

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
 const express = require('express');
 const morgan  = require('morgan');
 const playstore = require('./playstore');
+
+
+const app = express();
+
+app.use(morgan('common'));
+
+
+app.get('/apps', (req, res) => {
+    const {  sort, genre } = req.query;
+
+    if(sort) {
+        if(!['Rating', 'App'].includes(sort)){
+            return res
+                .status(400)
+                .send('Sort must be either Rating or App!')
+        }
+    }
+
+    if(genre) {
+        if(!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']) {
+            return res
+                .status(400)
+                .send('Genres must be either Action, Puzzle, Strategy, Casual, Arcade, or Card!')
+        }
+    }
+
+    let results = playstore
+        .filter(item =>
+            item);
+
+    if(sort) {
+        results
+            .sort((a,b) => {
+                return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+            })
+    }
+
+    if(genre) {
+        results
+            .filter(item => item.Genres.includes(genre) )
+    }
+
+    res
+        .json(results);
+})
+
+
+
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+})
